453953b612994bdc9592f18510095a65aa87c92f
Stark <deed34@yandex.ru>
2021-05-04 07:33:07 +0300

    refactor authorization system to cookie

--- classes/UserAccess.js
+++ classes/UserAccess.js
@@ -1,10 +1,9 @@
 import Router from 'next/router';
+import Cookie from 'js-cookie';
 
 import Fetch from 'classes/Fetch';
 
 export default class UserAccess {
-    static storageKey = 'user_access_data';
-
     static login(formData) {
         return new Fetch('/api/v1/oauth2/token', {
             method: 'POST',
@@ -24,15 +23,24 @@ export default class UserAccess {
         UserAccess.clear();
     }
 
+    static check() {
+        return !!Cookie.get('access_token');
+    }
+
     static get() {
-        return localStorage.getItem(UserAccess.storageKey);
+        return {
+            access_token: Cookie.get('access_token'),
+            refresh_token: Cookie.get('refresh_token')
+        };
     }
 
     static set(data) {
-        localStorage.setItem(UserAccess.storageKey, JSON.stringify(data));
+        Cookie.set('access_token', data.access_token, { expires: data.expires_in });
+        Cookie.set('refresh_token', data.refresh_token);
     }
 
     static clear() {
-        localStorage.removeItem(UserAccess.storageKey);
+        Cookie.remove('access_token');
+        Cookie.remove('refresh_token');
     }
 }
--- pages/personal/index.js
+++ pages/personal/index.js
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 
 import PersonalLayout from 'layouts/PersonalLayout';
+import Utils from 'classes/Utils';
 
 function Personal() { // todo
     return (
@@ -10,13 +11,19 @@ function Personal() { // todo
     );
 }
 
-export async function getServerSideProps() {
-    return {
-        redirect: {
-            destination: '/',
-            permanent: false,
-        }
-    };
+export async function getServerSideProps({ req }) {
+    const cookie = Utils.formatCookie(req.headers.cookie);
+
+    if (!cookie.access_token) {
+        return {
+            redirect: {
+                destination: '/404',
+                permanent: false,
+            }
+        };
+    }
+
+    return { props: {} };
 }
 
 export default dynamic(() => Promise.resolve(Personal), {
