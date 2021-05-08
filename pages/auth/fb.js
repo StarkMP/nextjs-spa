a136a1d3a3a047c7cc97c131c7da42cda0b30c00
Stark <deed34@yandex.ru>
2021-05-08 13:04:55 +0300

    add fb auth page error

--- pages/auth/fb.js
+++ pages/auth/fb.js
@@ -1,11 +1,21 @@
 import FormData from 'form-data';
+import Link from 'next/link';
 
 import Values from 'classes/Values';
 import Fetch from 'classes/Fetch';
 import Utils from 'classes/Utils';
 
 export default function FacebookAuth() {
-    return <p>Ошибка!</p>;
+    return (
+        <div className='confirm confirm_error'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='120' height='120' fill='currentColor' className='mb-5 bi bi-x-circle' viewBox='0 0 16 16'>
+                <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>
+                <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+            </svg>
+            <p className='fs-5'>Что-то пошло не так!</p>
+            <Link href='/personal/instapages/create'><a>Попробуйте ещё раз</a></Link>
+        </div>
+    );
 }
 
 export async function getServerSideProps({ query, req }) {
@@ -32,8 +42,6 @@ export async function getServerSideProps({ query, req }) {
 
         return Values.serverRedirect(`/personal/instapages/create/constructor?login=${json.login}`);
     } catch(err) {
-        console.error(err);
-
         return Values.emptyProps;
     }
 }
